bf73964b76ebf430669d452592b91f65572a5a60
chenchaoyun <chenchaoyun@sttxtech.com>
2017-08-23 22:02:04 +0800

    modify config
    Change-Id: Ieef5f84f855373797d49326274f8a1589a90d1ad

--- src/main/java/com/sttx/bookmanager/conf/ConfigClass.java
+++ src/main/java/com/sttx/bookmanager/conf/ConfigClass.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@ImportResource(locations = { "classpath:bookmanagerApp.xml" })
+@ImportResource(locations = { "classpath:bookmanagerApp.xml", "classpath:spring/springmvc.xml" })
 public class ConfigClass {
 
     public ConfigClass() {
