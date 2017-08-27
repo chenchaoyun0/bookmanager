53a6a360dc4206ec24325309c7e1b3e67c155696
chenchaoyun <chenchaoyun@sttxtech.com>
2017-08-27 21:34:46 +0800

    modify the log pages default pageSize 8
    Change-Id: I4dd680663b54ec7875f6352a9c94a18b379d056b

--- src/main/java/com/sttx/bookmanager/service/impl/LogServiceImpl.java
+++ src/main/java/com/sttx/bookmanager/service/impl/LogServiceImpl.java
@@ -63,7 +63,7 @@ public class LogServiceImpl implements ILogService {
          * 默认是12条
          */
         pageNo = pageNo == null ? (Integer) 1 : pageNo;
-        pageSize = pageSize == null ? (Integer) 20 : pageSize;
+        pageSize = pageSize == null ? (Integer) 8 : pageSize;
         PageHelper.startPage(pageNo, pageSize);// 告诉插件开始分页
         List<TLog> selectLogPages = tLogMapper.selectLogPages(tLog);
         PagedResult<TLog> logPagedResult = BeanUtil.toPagedResult(selectLogPages);
