1cf42a115e18852c631a8931b85258a03bd24e58
chenchaoyun <chenchaoyun@sttxtech.com>
2017-08-28 22:11:36 +0800

    modify druid ini connection size
    Change-Id: I937af62fc01136571ecc4f9b42b43d3fe614b978

--- src/test/java/com/sttx/bookmanager/post/TestThreadPool.java
+++ src/test/java/com/sttx/bookmanager/post/TestThreadPool.java
@@ -13,13 +13,13 @@ public class TestThreadPool {
     public static void main(String[] args) {
         MyPoolableObjectFactory factory = new MyPoolableObjectFactory();
         GenericObjectPool pool = new GenericObjectPool(factory);
-        pool.setMaxActive(300); // 能从池中借出的对象的最大数目
-        pool.setMaxIdle(20); // 池中可以空闲对象的最大数目
+        pool.setMaxActive(50000); // 能从池中借出的对象的最大数目
+        pool.setMaxIdle(100); // 池中可以空闲对象的最大数目
         pool.setMaxWait(100); // 对象池空时调用borrowObject方法，最多等待多少毫秒
         pool.setTimeBetweenEvictionRunsMillis(600000);// 间隔每过多少毫秒进行一次后台对象清理的行动
         pool.setNumTestsPerEvictionRun(-1);// －1表示清理时检查所有线程
         pool.setMinEvictableIdleTimeMillis(3000);// 设定在进行后台对象清理时，休眠时间超过了3000毫秒的对象为过期
-        for (int i = 0; i < 250; i++) {
+        for (int i = 0; i < 10000; i++) {
             try {
                 SendPostThread simpleThread = (SendPostThread) pool.borrowObject();
                 simpleThread.setPool(pool);
