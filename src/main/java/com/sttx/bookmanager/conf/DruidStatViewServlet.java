81c0583873aba2434cbb48b1c721c61179721a77
chenchaoyun <chenchaoyun@sttxtech.com>
2017-08-29 19:34:29 +0800

    modify druid auth
    Change-Id: Ia4dfcf19261953dfcba4e758999ffe2b52675364

--- src/main/java/com/sttx/bookmanager/conf/DruidStatViewServlet.java
+++ src/main/java/com/sttx/bookmanager/conf/DruidStatViewServlet.java
@@ -12,9 +12,9 @@ import com.alibaba.druid.support.http.StatViewServlet;
         /** 黑名单，与白名单存在相同IP时，优先于白名单 */
         @WebInitParam(name = "deny", value = ""),
         /** 用户名 */
-        @WebInitParam(name = "loginUsername", value = "chenchaoyun"),
+        // @WebInitParam(name = "loginUsername", value = "chenchaoyun"),
         /** 密码 */
-        @WebInitParam(name = "loginPassword", value = "111111"),
+        // @WebInitParam(name = "loginPassword", value = "111111"),
         /** 禁用HTML页面上的“Reset All”功能 */
         @WebInitParam(name = "resetEnable", value = "false") })
 public class DruidStatViewServlet extends StatViewServlet {
