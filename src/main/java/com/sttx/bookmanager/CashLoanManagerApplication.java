904bc907a35f5612d56bf0f51749b0160dba7d32
chenchaoyun <chenchaoyun@megvii.com>
2018-03-03 22:08:03 +0800

    更新springboot 为2.0
    Change-Id: Id07be6189aa23dec71039360483fc0f3373ae08f

--- src/main/java/com/sttx/bookmanager/CashLoanManagerApplication.java
+++ src/main/java/com/sttx/bookmanager/CashLoanManagerApplication.java
@@ -2,27 +2,22 @@ package com.sttx.bookmanager;
 
 import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
-
 import javax.sql.DataSource;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
-
 import com.sttx.bookmanager.util.spring.SpringUtils;
 
 @SpringBootApplication
 @ServletComponentScan
 public class CashLoanManagerApplication extends SpringBootServletInitializer
-        implements EmbeddedServletContainerCustomizer, ApplicationListener<ContextRefreshedEvent> {
+        implements  ApplicationListener<ContextRefreshedEvent> {
     private static final Logger log = LoggerFactory.getLogger(CashLoanManagerApplication.class);
     private static String contextPath = null;
     private static String port = null;
@@ -35,16 +30,6 @@ public class CashLoanManagerApplication extends SpringBootServletInitializer
     public static void main(String[] args) throws Exception {
         SpringApplication.run(CashLoanManagerApplication.class, args);
         log.info(">>>>>图书管理系统启动成功!");
-    }
-
-    @Override
-    public void customize(ConfigurableEmbeddedServletContainer container) {
-        if (contextPath != null) {
-            container.setContextPath(contextPath);
-        }
-        if (port != null) {
-            container.setPort(Integer.valueOf(port));
-        }
     }
 
     @Override
