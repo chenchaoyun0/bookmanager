2fa993f4328229096c1ee359cae7031211247c37
chenchaoyun <chenchaoyun@megvii.com>
2018-03-17 00:09:36 +0800

    修改主页日志查询sql
    Change-Id: Ib96a9f7fd09b832aa3dac57afbfa4e42a8212b95

--- src/main/java/com/sttx/bookmanager/web/controller/IndexHomeController.java
+++ src/main/java/com/sttx/bookmanager/web/controller/IndexHomeController.java
@@ -54,7 +54,7 @@ public class IndexHomeController {
         log.info(">>>>>>>>>realPath:{}",realPath);
         // return "forward:/book/selectBookPages";
         TLog tLog = new TLog();
-        PagedResult<TLog> pages = logService.selectLogPagesByMongo(tLog, pageNo, pageSize);
+        PagedResult<TLog> pages = logService.selectLogPages(tLog, pageNo, pageSize);
         Long totalcount = logService.selectLogSumCount();
         String url = request.getRequestURI();
         pages.setUrl(url);
@@ -88,7 +88,7 @@ public class IndexHomeController {
     public String indexHomeForIp(String userIp, Model model, HttpServletRequest request, ModelAndView modelAndView,
             Integer pageNo, Integer pageSize) {
         //        return "forward:/book/selectBookPages";
-        PagedResult<TLog> pages = logService.selectLogPagesForIpByMongo(userIp, pageNo, pageSize);
+        PagedResult<TLog> pages = logService.selectLogPagesForIp(userIp, pageNo, pageSize);
         Long totalcount = logService.selectLogSumCount();
         String url = request.getRequestURI();
         pages.setStrWhere("userIp=" + userIp);
