4835505f109474189f23367cf4c42a38ae5a2c0b
chenchaoyun <chenchaoyun@sttxtech.com>
2018-05-17 14:28:43 +0800

    copy book
    Change-Id: I4164ce3e18cf9d8307e7c1dcba474209586f8c8a

--- src/test/java/com/sttx/bookmanager/test/BookTest.java
+++ src/test/java/com/sttx/bookmanager/test/BookTest.java
@@ -1,5 +1,7 @@
 package com.sttx.bookmanager.test;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -9,10 +11,16 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import com.sttx.bookmanager.dao.BookMapper;
 import com.sttx.bookmanager.po.Book;
+import com.sttx.bookmanager.po.TImg;
 import com.sttx.bookmanager.po.User;
 import com.sttx.bookmanager.service.IBookService;
+import com.sttx.bookmanager.service.IImgService;
+import com.sttx.bookmanager.util.BookManagerBeanUtils;
 import com.sttx.bookmanager.util.pages.PagedResult;
+
+import cn.itcast.commons.CommonUtils;
 
 @ContextConfiguration(locations = { "classpath:spring/applicationContext-*.xml" })
 @RunWith(SpringJUnit4ClassRunner.class) // SpringJUnit支持，由此引入Spring-Test框架支持！
@@ -21,7 +29,10 @@ public class BookTest {
   private static final Logger logger = LoggerFactory.getLogger(BookTest.class);
     @Autowired
     private IBookService bookService;
-
+  @Autowired
+  private IImgService imgService;
+  @Autowired
+  private BookMapper bookMapper;
     @Test
     public void testSelectByPrimaryKey() {
 
@@ -54,4 +65,26 @@ public class BookTest {
         logger.info("testUnmountBook:{}:" + i);
     }
 
+  @Test
+  public void testSelectCopyBook() {
+    for (int i = 0; i < 100; i++) {
+      List<Book> dataList = bookMapper.selectBookPages(new Book());
+      for (Book book : dataList) {
+        Book copyBean2 = BookManagerBeanUtils.copyBean(book, Book.class);
+        String uuid2 = CommonUtils.uuid();
+        copyBean2.setBookId(uuid2);
+        TImg tImg = new TImg();
+        tImg.setLinkId(book.getBookId());
+        List<TImg> imgList = imgService.selectList(tImg);
+        for (TImg tImg2 : imgList) {
+          TImg copyBean = BookManagerBeanUtils.copyBean(tImg2, TImg.class);
+          String uuid = CommonUtils.uuid();
+          copyBean.setImgId(uuid);
+          copyBean.setLinkId(uuid2);
+          imgService.insert(copyBean);
+        }
+        bookService.insertSelective(copyBean2);
+      }
+    }
+  }
 }
